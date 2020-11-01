f0241237b3b78b7794cfb6e0aa8cfc720d57eacb
toriving <toriving@gmail.com>
2020-11-02 00:35:37 +0900

    Update project

--- project/pybo/views/main_views.py
+++ project/pybo/views/main_views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
+
 
 @bp.route('/hello')
 def hello_pybo():
@@ -9,4 +11,4 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
-    return 'Pybo index'
+    return redirect(url_for('question._list'))
