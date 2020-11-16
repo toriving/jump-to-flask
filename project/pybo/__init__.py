4111684b221b4fd08564c90f3fceb86c5d8f3623
toriving <toriving@gmail.com>
2020-11-17 01:43:50 +0900

    Update chapter 3-15

--- project/pybo/__init__.py
+++ project/pybo/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 from flaskext.markdown import Markdown
+from sqlalchemy import MetaData
 
 import config
 
@@ -16,20 +16,20 @@ naming_convention = {
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
-    # markdown
-    Markdown(app, extensions=['nl2br', 'fenced_code'])
+
     # ORM
     db.init_app(app)
     if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
         migrate.init_app(app, db, render_as_batch=True)
     else:
         migrate.init_app(app, db)
-
     from . import models
 
+    # 블루프린트
     from .views import main_views, question_views, answer_views, auth_views, comment_views, vote_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
@@ -38,7 +38,11 @@ def create_app():
     app.register_blueprint(comment_views.bp)
     app.register_blueprint(vote_views.bp)
 
+    # 필터
     from .filter import format_datetime
     app.jinja_env.filters['datetime'] = format_datetime
+
+    # markdown
+    Markdown(app, extensions=['nl2br', 'fenced_code'])
 
     return app
